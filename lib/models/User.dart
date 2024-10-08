1bdc11aee9b12e9b71baaf5df4ecc68a9effcca5
Thibaud-LAMON <thib.lamon@gmail.com>
2024-10-08 10:07:19 +0200

    User model

---
+++ lib/models/User.dart
@@ -0,0 +1,22 @@
+class User {
+  int? userId;
+  String name;
+  String email;
+  String password;
+  //Cabinet? cabinetId;
+  DateTime createdAt;
+
+  User({
+    this.userId,
+    required this.name,
+    required this.email,
+    required this.password,
+    //this.cabinetId,
+    required this.createdAt,
+  });
+
+  @override
+  String toString() {
+    return 'User{userId: $userId, name: $name, email: $email, password: $password, createdAt: $createdAt}';
+  }
+}
